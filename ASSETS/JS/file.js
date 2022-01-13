708768904daa9eac4c246f2efe9fa3a306604583
Domenico <94327171+Domecosma@users.noreply.github.com>
2022-01-13 17:44:51 +0100

    Update file.js

--- ASSETS/JS/file.js
+++ ASSETS/JS/file.js
@@ -14,6 +14,67 @@ if (name == '' || name == null || name == undefined) {
     title.innerHTML = 'THE COUNTER ';
 }
 
+// div the counter value
+
+const divCounter = document.querySelector('.counter');
+const counterValue = document.createElement('div');
+counterValue.setAttribute('class', 'counter-value')
+counterValue.textContent = '0';
+divCounter.appendChild(counterValue);
+
+const div = document.querySelector('div');
+
+
+// button decrease
+
+const btnDecrease = document.createElement('button');
+btnDecrease.setAttribute('id', 'button-1')
+btnDecrease.setAttribute('data-action', 'subtract')
+btnDecrease.textContent = 'DECREASE';
+div.appendChild(btnDecrease);
+
+// button reset
+
+const btnReset = document.createElement('button');
+btnReset.setAttribute('data-action', 'reset')
+btnReset.textContent = 'RESET';
+div.appendChild(btnReset);
+
+
+// button increase
+
+const btnIncrease = document.createElement('button');
+btnIncrease.setAttribute('id', 'button-2')
+btnIncrease.setAttribute('data-action', 'add')
+btnIncrease.textContent = 'INCREASE';
+div.appendChild(btnIncrease);
+
+
+// div class secondary
+
+const divSecondary = document.querySelector('.secondary-controls');
+
+// Button decrease -2
+
+const btnDecrease2 = document.createElement('button');
+btnDecrease2.setAttribute('id', 'button-3')
+btnDecrease2.setAttribute('data-action', 'subtract-twice')
+btnDecrease2.textContent = 'DECREASE-2';
+div.appendChild(btnDecrease2);
+
+// Button Increase +2
+
+const btnIncrease2 = document.createElement('button');
+btnIncrease2.setAttribute('id', 'button-4')
+btnIncrease2.setAttribute('data-action', 'add-twice')
+btnIncrease2.textContent = 'INCREASE-2';
+div.appendChild(btnIncrease2);
+
+
+
+
+
+
 
 
 
@@ -22,7 +83,7 @@ let counter = 0;
 let actions = Array.from(document.querySelectorAll('[data-action]'));
 
 
-document.querySelector('.counter-value').innerText = counter;
+
 
 
 actions.forEach(action => {
@@ -47,12 +108,11 @@ actions.forEach(action => {
                 break;
         }
 
-        document.querySelector('.counter-value').innerText = counter;
+        document.querySelector('.counter-value').textContent = counter;
 
     })
 
 })
-
 
 
 
